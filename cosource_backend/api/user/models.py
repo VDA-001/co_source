02cf4d904bac90f003eab6fb35d12660cc67a754
VDA-001 <vdavishu@gmail.com>
2021-05-28 21:38:09 +0530

    Added extra fields to user model

--- cosource_backend/api/user/models.py
+++ cosource_backend/api/user/models.py
@@ -9,6 +9,9 @@ class CustomUser(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     phone = models.CharField(max_length=15,blank = True,null=True)
+    is_volunteer = models.BooleanField()
+    country = models.CharField(max_length=30,null=False)
+    region = models.CharField(max_length=30,null=False)
 
     objects = UserManager()
 
