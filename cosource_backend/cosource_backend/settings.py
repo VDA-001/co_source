5a6e5b5ce946e1dfdbb8b435c1b159391e34c4d5
VDA-001 <vdavishu@gmail.com>
2021-05-28 14:25:48 +0530

    Generated new secret key

--- cosource_backend/cosource_backend/settings.py
+++ cosource_backend/cosource_backend/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1@1ld3i_y_*asurkwypm9mcs&x#od-sm)ued0q&#j5fpja7qg)'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
